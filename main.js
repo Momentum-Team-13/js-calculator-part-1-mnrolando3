7ca05d76b7ebd517b38b00ed991b2bf218c26f4e
Metta Rolando <mnrolando3@gmail.com>
2022-05-27 11:50:24 -0400

    Simplified result code

--- main.js
+++ main.js
@@ -14,13 +14,20 @@ const result = document.querySelector("#result")
 
 for (let key of keys) {
     key.addEventListener("click", function (event) {
-        console.log(event.target.textContent);
-        let resultBox = document.createElement("div")
+        // console.log(event.target.textContent);
+        // let resultBox = document.createElement("div")
         //made new element
-        resultBox.innerText = event.target.textContent;
+        // resultBox.innerText = event.target.textContent;
         //updated the inner text of that new element to be the contents of the clicked box
-        result.appendChild(resultBox);
+        // result.appendChild(resultBox);
         //append the new element to the currently empty result element
+
+        result.innerText += event.target.textContent;
+        //writing text content of target where event occurred (aka number of key that was clicked) into result (defined above)
+
+        let num1 = event.target.textContent;
+        let operation = document.querySelector(".operation")
+        let num2 = event.target.textContent; {}
     });
 };
 
@@ -32,8 +39,17 @@ clear.addEventListener("click", function (event) {
 
 // clearButton.remove(resultBox);
 
-// const list = document.getElementById("myList");
 
-// while (result.hasChildNodes()) {
-//     result.removeChild(result.firstChild);
+// function multigreeting(name, lang) {
+//     if (lang === "en") {
+//         return "Hello, " + name + "!";
+//     } else if (lang === "es") {
+//         return "¡Hola, " + name + "!";
+//     } else if (lang === "fr") {
+//         return "Bonjour, " + name + "!";
+//     } else if (lang === "eo") {
+//         return "Saluton, " + name + "!";
+//     } else {
+//         return;
+//     }
 // }
