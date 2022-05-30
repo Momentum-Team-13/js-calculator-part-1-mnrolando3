e4e02eb2e761e1b8c179612666b12f172edd7a0b
Metta Rolando <mnrolando3@gmail.com>
2022-05-29 21:19:42 -0400

    Updated eval to math.eval, referencing math library. Updated html to include additional operation keys.

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const result = document.querySelector("#result")
 for (let key of keys) {
     key.addEventListener("click", function (event) {
         if (event.target.textContent === "=") {
-            result.innerText = eval(result.textContent).toPrecision(10);
+            result.innerText = math.eval(result.textContent);
+            //     if (result.textContent.length >= 11) {
+            //         result.innerText = result.textContent.toPrecision(10);
+            //     }
+            // }
         } else if (event.target.textContent === "C") {
             result.innerText = ""
         } else {
