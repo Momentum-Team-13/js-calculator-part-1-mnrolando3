e346eac1679ce94954ad4969c251be91086f34d4
Metta Rolando <mnrolando3@gmail.com>
2022-05-31 08:55:00 -0400

    Updated math to show only 8 sigfigs if length of result string is greater than 11.

--- main.js
+++ main.js
@@ -5,19 +5,11 @@ const result = document.querySelector("#result")
 for (let key of keys) {
     key.addEventListener("click", function (event) {
         if (event.target.textContent === "=") {
-            // if (result.textContent.length >= 11) {
-            //     result.innerText = math.eval(result.textContent).toPrecision(10);
-            // }
-            // need to build if clause for evaluation being greater than or equal to 11 digits, then make it a string to check the length/find function to check length of number, then proceed
-            // console.log(result.innerText);
-            result.innerText = math.eval(result.textContent);
-            // math.eval(result.textContent);
-            // result.innerText = answer
-
-            //     if (result.textContent.length >= 11) {
-            //         result.innerText = result.textContent.toPrecision(10);
-            //     }
-            // }
+            result.innerText = math.eval(result.textContent); {
+                if (result.innerText.length >= 11) {
+                    result.innerText = math.eval(result.textContent).toPrecision(8);
+                }
+            };
         } else if (event.target.textContent === "C") {
             result.innerText = ""
         } else {
